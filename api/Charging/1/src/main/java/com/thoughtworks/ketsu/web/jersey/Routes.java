90fa4e8587931175a5fff54d569d354f7e15a102
SigridZhangSY <sigridzhang0603@163.com>
2016-11-29 00:56:38 +0800

    init

---
+++ api/Charging/1/src/main/java/com/thoughtworks/ketsu/web/jersey/Routes.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.ketsu.web.jersey;
+
+
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+
+public class Routes {
+
+    private final String baseUri;
+
+    public Routes(UriInfo uriInfo) {
+        baseUri = uriInfo.getBaseUri().toASCIIString();
+    }
+
+//    public URI userUrl(User user) {
+//        return URI.create(String.format("%susers/%s", baseUri, user.getUserId().id()));
+//    }
+}
