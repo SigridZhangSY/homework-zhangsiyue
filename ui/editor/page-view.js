6101d3dc095e88b69cf567022f94c4d93d57b7e0
SigridZhangSY <sigridzhang0603@163.com>
2016-12-01 17:28:29 +0800

    edit button click function

--- ui/editor/page-view.js
+++ ui/editor/page-view.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
-    view();
+    render();
+    editClick();
 });
 
-var view = function () {
-    $('.title').each(function (title) {
-        $(this).children('div.component').replaceWith("<div class='component'><h1>" + $(this).attr("data") + "</h1></div>");
+var render = function () {
+    $('.title').each(function () {
+        $(this).children('div.component').replaceWith("<div class='component'><h1 class='render'>" + $(this).attr("data") + "</h1></div>");
     });
 
-    $('.text').each(function (text) {
-        $(this).children('div.component').replaceWith("<div class='component'>" + $(this).attr("data") + "</div>");
+    $('.text').each(function () {
+        $(this).children('div.component').replaceWith("<div class='component'><p class='render'>" + $(this).attr("data") + "</p></div>");
     });
 };
+
+var editClick = function () {
+    $('#edit-button').each(function () {
+        $(this).click(
+            function () {
+                // alert("xxx");
+                $('.title').each(function () {
+                    $(this).children('div.component').replaceWith("<div class='component'><input class='edit' value='" + $(this).attr("data") + "'/></div>");
+                });
+
+                $('.text').each(function () {
+                    $(this).children('div.component').replaceWith("<div class='component'><input class='edit' value='" + $(this).attr("data") + "'/></div>");
+                });
+            }
+        );
+    });
+}
