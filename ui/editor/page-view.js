35bdfe61bd4a6b84672600e1de10febdfc43ad8e
SigridZhangSY <sigridzhang0603@163.com>
2016-12-01 17:35:46 +0800

    render button click function

--- ui/editor/page-view.js
+++ ui/editor/page-view.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     render();
     editClick();
+    renderClick();
 });
 
 var render = function () {
@@ -24,6 +25,23 @@ var editClick = function () {
 
                 $('.text').each(function () {
                     $(this).children('div.component').replaceWith("<div class='component'><input class='edit' value='" + $(this).attr("data") + "'/></div>");
+                });
+            }
+        );
+    });
+}
+
+var renderClick = function () {
+    $('#render-button').each(function () {
+        $(this).click(
+            function () {
+                // alert("xxx");
+                $('.title').each(function () {
+                    $(this).children('div.component').replaceWith("<div class='component'><h1 class='render'>" + $(this).attr("data") + "</h></div>");
+                });
+
+                $('.text').each(function () {
+                    $(this).children('div.component').replaceWith("<div class='component'><p class='render'>" + $(this).attr("data") + "</p></div>");
                 });
             }
         );
