110c50fa9746cf086f6413d7120a2c6e8b551232
SigridZhangSY <sigridzhang0603@163.com>
2016-12-07 16:04:08 +0800

    add component

--- ui/editor/page-view.js
+++ ui/editor/page-view.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     editClick();
     renderClick();
+    addClick();
     move('.title');
     move('.text');
 });
@@ -44,32 +45,44 @@ var renderClick = function () {
 };
 
 var move = function (selector) {
-    $(selector).mousedown(
-        function (event) {
-            var isMove = true;
-            var y = $(this).offset().top;
-            var abs_x = event.pageX - $(this).offset().left;
-            var abs_y = event.pageY - $(this).offset().top;
-            var others = $(this).siblings();
-
-            $(this).mousemove(function (event) {
-                    if (isMove) {
-                        $(this).css({'left': event.pageX - abs_x, 'top': event.pageY - abs_y, 'position': 'absolute'});
-                    }
+    $(selector).each(function () {
+            $(this).mousedown(
+                function (event) {
+                    var isMove = true;
+                    var y = $(this).offset().top;
+                    var abs_x = event.pageX - $(this).offset().left;
+                    var abs_y = event.pageY - $(this).offset().top;
+                    console.log(this.className)
+                    $(this).mousemove(function (event) {
+                            if (isMove) {
+                                $(this).css({'left': event.pageX - abs_x, 'top': event.pageY - abs_y, 'position': 'absolute'});
+                            }
+                        }
+                    ).mouseup(
+                        function () {
+                            isMove = false;
+                            var next_bottom = $(this).nextAll().length !== 0 ? $(this).next().offset().top + $(this).next().height() : -1;
+                            var pre_top = $(this).prevAll().length !== 0 ? $(this).prev().offset().top : -1;
+                            if ($(this).offset().top - y > 0 && next_bottom >= 0 && $(this).offset().top > next_bottom)
+                                $(this).next().insertBefore(this);
+                            if ($(this).offset().top - y < 0 && pre_top >= 0 && $(this).offset().top < pre_top)
+                                $(this).insertBefore($(this).prev());
+                            $(this).removeAttr('style');
+                        }
+                    );
                 }
-            ).mouseup(
-                function () {
-                    isMove = false;
-                    var next_bottom = $(this).nextAll().length !==0 ? $(this).next().offset().top + $(this).next().height(): -1;
-                    var pre_top = $(this).prevAll().length !== 0? $(this).prev().offset().top : -1;
-                    // console.log(this.className + ' ' + $(this).offset().top+' '+ y + ' ' + pre_top + ' ' + next_bottom);
-                    if ($(this).offset().top - y > 0 && next_bottom >= 0 && $(this).offset().top > next_bottom)
-                        $(this).next().insertBefore(this);
-                    if ($(this).offset().top - y < 0 && pre_top >= 0 && $(this).offset().top < pre_top)
-                        $(this).insertBefore($(this).prev());
-                    $(this).removeAttr('style');
-                }
-            );
+            )
         }
     );
+};
+
+var addClick = function () {
+    $('#add-button').each(function () {
+        $(this).click(
+            function () {
+                var innerHtml = '<li class="text area add" data="content"><div class="component"><p class="render">content</p></div></li>';
+                $('.content-area').append(innerHtml);
+            }
+        );
+    });
 }
