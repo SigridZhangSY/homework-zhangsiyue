eebb064860837f15fad8b7aa7879a5563520646d
SigridZhangSY <sigridzhang0603@163.com>
2016-12-16 11:18:25 +0800

    [ioc/1] refactor: remove new instance logic back to resolve

--- basic/ioc/1/README.md
+++ basic/ioc/1/README.md
@@ -7,3 +7,9 @@ Implement a simple container with annotation in java
 2. bind implement and interface -10min 7min
 
 3. should not bind class to interface which it not implement -10min  30min
+
+4. refactor 
+    - record map relationship in bind, remove new instance logic back to resolve -10min 13min
+    - resolve a class container not instantiate -7min 7min
+    - resolve a class container instantiated -7min
+ 
--- basic/ioc/1/src/container/Container.java
+++ basic/ioc/1/src/container/Container.java
@@ -1,26 +1,33 @@
 package container;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Container<K,V> {
 
-    Map<Class<K>, V > dictionary = new HashMap<>();
+    Map<Class<K>, Class<V>>  dictionary = new HashMap<>();
+    Set<V> instances = new HashSet<>();
 
     public void bind(Class<K> interfaceType, Class<V> implementType) throws Exception{
+
         Class[] interfaces = implementType.getInterfaces();
+
         if (Arrays.asList(interfaces).contains(interfaceType))
-            dictionary.put(interfaceType, (V)implementType.newInstance());
+            dictionary.put(interfaceType, implementType);
         else
             throw new Exception();
     }
 
     public V resolve(Class<K> type) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        V implement = dictionary.get(type);
-        if(implement != null)
-            return implement;
-        else
-            return null;
+        Class<V> implementType = dictionary.get(type);
+
+        if(implementType != null){
+            Optional<V> implement = instances.stream().filter(instance -> instance.getClass() == implementType ).findAny();
+            if(!implement.isPresent()){
+                implement = Optional.of(implementType.newInstance());
+                instances.add(implement.get());
+            }
+            return implement.get();
+        }
+        return null;
     }
 }
