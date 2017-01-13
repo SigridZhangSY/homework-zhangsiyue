9679785d0f749384ea7ce79e777301bfd2ceca76
syzhang <syzhang@thoughtworks.com>
2017-01-13 12:53:03 +0800

    clean

--- basic/rich/rich-1/test/rich/commandHandler/WaitCommandHandlerUseToolTest.java
+++ basic/rich/rich-1/test/rich/commandHandler/WaitCommandHandlerUseToolTest.java
@@ -95,7 +95,6 @@ public class WaitCommandHandlerUseToolTest {
 
         player.executed("bomb 1");
 
-        System.out.println("test new branch");
         assertThat(player.getHandler() instanceof WaitCommandHandler, is(true));
         assertThat(player.getTools().size(), is(1));
         assertThat(target.isBombed(), is(false));
