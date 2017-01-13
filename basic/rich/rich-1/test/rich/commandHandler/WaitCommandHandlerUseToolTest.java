1d779301e244f4e8e86ef81212d42b01f3119e07
syzhang <syzhang@thoughtworks.com>
2017-01-13 12:53:58 +0800

    add new branch

--- basic/rich/rich-1/test/rich/commandHandler/WaitCommandHandlerUseToolTest.java
+++ basic/rich/rich-1/test/rich/commandHandler/WaitCommandHandlerUseToolTest.java
@@ -95,6 +95,8 @@ public class WaitCommandHandlerUseToolTest {
 
         player.executed("bomb 1");
 
+        System.out.println("test new branch");
+
         assertThat(player.getHandler() instanceof WaitCommandHandler, is(true));
         assertThat(player.getTools().size(), is(1));
         assertThat(target.isBombed(), is(false));
